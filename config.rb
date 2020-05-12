47fc70ce289cb24114b1db2ecb85cbd49830aeda
Masanori HIRANO <masanorihirano@users.noreply.github.com>
2020-05-12 12:24:56 +0900

    update repositories

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 DRIVES = {
 	"hirano" => "0ALL-IXlxwkOjUk9PVA",
 	"nlp" => "0AAV8YJ9vNJ9AUk9PVA",
-	"row" => "0AAJregQXee8IUk9PVA"
+	"row" => "0AAJregQXee8IUk9PVA",
+	"flex_std_processed" => "0AN26S1MNn6QTUk9PVA"
 }
 
