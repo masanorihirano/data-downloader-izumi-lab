a060634703c5d4cb582f274d20b30e94742d2c39
Masanori HIRANO <masanorihirano@users.noreply.github.com>
2020-05-22 19:04:20 +0900

    added repository

--- config.rb
+++ config.rb
@@ -2,6 +2,7 @@ DRIVES = {
 	"hirano" => "0ALL-IXlxwkOjUk9PVA",
 	"nlp" => "0AAV8YJ9vNJ9AUk9PVA",
 	"row" => "0AAJregQXee8IUk9PVA",
-	"flex_std_processed" => "0AN26S1MNn6QTUk9PVA"
+	"flex_std_processed" => "0AN26S1MNn6QTUk9PVA",
+	"flex_std_processed_divided" => "0AJlJN2ylAFFjUk9PVA",
 }
 
